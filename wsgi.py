35d37b4f4ccbec12a8682d0e20b3cb5f44b10add
mostafa-anm <mostafa.anm91@gmail.com>
2016-02-18 19:13:50 +0100

    wip

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,12 @@
+import os
 import static
+from wsgioauth2 import google
 
 application = static.Cling('/app/docs/_build/html')
+
+key = os.environ.get('DJANGO_SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
+secret = os.environ.get('DJANGO_SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
+
+client = google.make_client(client_id=key, client_secret=secret, access_type="offline",
+scope="https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile")
+application = client.wsgi_middleware(application, secret=secret.encode('utf-8'))
