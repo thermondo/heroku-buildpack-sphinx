a367ffe10c90d67d896b41fb9a39bf073b1ea5bb
mostafa-anm <mostafa.anm91@gmail.com>
2016-02-19 11:09:20 +0100

    Adds static auth endpoint

--- wsgi.py
+++ wsgi.py
@@ -8,5 +8,5 @@ key = os.environ.get('DJANGO_SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
 secret = os.environ.get('DJANGO_SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
 
 client = google.make_client(client_id=key, client_secret=secret, hd="thermondo.de",
-scope="https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile")
-application = client.wsgi_middleware(application, secret=secret.encode('utf-8'))
+scope="https://www.googleapis.com/auth/userinfo.email")
+application = client.wsgi_middleware(application, secret=secret.encode('utf-8'), path='/oauth2/')
